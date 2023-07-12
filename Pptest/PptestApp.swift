69a29d8bc5467e19722993a41cacc1422423a3b0
YoungBin Choi <chldudqlsdl@naver.com>
2023-07-12 16:33:41 +0900

    Initial Commit

---
+++ Pptest/PptestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PptestApp.swift
+//  Pptest
+//
+//  Created by Youngbin Choi on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct PptestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
